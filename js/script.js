8e9c173ffbfe08c9bc6d2742af15d25cdd004034
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-04-05 18:17:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-
 $(function(){
 
   var carouselList = $("#slider ul");
-  setInterval(changeSlide, 4000);
-  function changeSlide(){
-    carouselList.animate({'marginLeft':-600}, 500, moveFirstSlide)
+  setInterval(changeSlidePrev, 4000);
+  function changeSlidePrev(){
+    carouselList.animate({'marginLeft':-1200}, 500, moveFirstSlide)
   };
+ 
   
   function moveFirstSlide(){
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
+    carouselList.css({marginLeft:-600});
   };
 
-
-  $(".next").click(changeSlide);
+  function changeSlideNext(){
+    carouselList.animate({'marginLeft':0}, 500, moveLastSlide)
+  };
+ 
   
+  function moveLastSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft:-600});
+  };
+
+  $(".prev").click(changeSlidePrev);
+  $(".next").click(changeSlideNext);
   
 });
